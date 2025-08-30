fixed scaling using direct vggt

sonata/compute_scale_factor.py
@@ -39,7 +39,7 @@ def scale_coord(frame_distance: float, min_depth) -> float:
 
 if __name__ == "__main__":
     # Load predictions
-    point = torch.load("sonata_points.pt")
+    point = torch.load("../vggt/point_cloud_img1.pt")
     print(point.keys())
 
     name = torch.load("name.pt")
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     frame_distance = 1.45  # modify if needed
 
     # get overall min floor coordinate compute scale factor
-    floor_coords = get_coords_by_class(point, "floor", name)
+    floor_coords = torch.tensor(point["coord"]).cpu().detach().numpy()
     print(
         f"\nMax floor coords: {max(floor_coords[:, 1])}, min floor coords: {min(floor_coords[:, 1])}"
     )

sonata/get_target_distance.py
@@ -30,15 +30,15 @@ if __name__ == "__main__":
     print(
         f"\nMax {target} coords: {max(target_coords[:, 1])}, min chair coords: {min(target_coords[:, 1])}"
     )
-    max_index = np.argmax(abs(target_coords[:, 1]))
-    max_coord = target_coords[max_index]
-    print(f"Original max {target} coord: {max_coord}")
+    min_index = np.argmin(abs(target_coords[:, 1]))
+    min_coord = target_coords[min_index]
+    print(f"Original max {target} coord: {min_coord}")
 
-    target_forward_dis = max_coord[1] 
-    xy_target = max_coord[:2]
+    target_forward_dis = min_coord[1] 
+    xy_target = min_coord[:2]
     print(f"Calibrated {target} coord xyz coordinate: {xy_target}")
 
-    target_right_dis = max_coord[0]
+    target_right_dis = min_coord[0]
 
     # Write distance ouput to dis_output.py
     output_path = os.path.abspath(

vggt/vggt_inference.py
@@ -41,12 +41,11 @@ for img in image_names:
     h, w, _ = im.shape
     print(f"Image height: {h}, width: {w}")
 
-    im.resize((int(w / 2), int(h / 2)))
+    #im.resize((int(w / 2), int(h / 2)))
     images.append(img)
     print(f"Image {img} resized to: {int(h / 2)}x{int(w / 2)}")
 
 images = load_and_preprocess_images(images).to(device)
-
 
 with torch.no_grad():
     with torch.cuda.amp.autocast(dtype=dtype):
@@ -146,11 +145,14 @@ def convert_vggt_to_sonata(
         "color": torch.from_numpy(colors_all).float(),
     }
 
-    return sonata_dict
+    image1={"coord": coords_cropped[0], "normal": normals_list[0], "color": colors_cropped[0]}
+
+    return sonata_dict, image1
 
 
 # Call function convert VGGT point map to SONATA format
-Sonata_format = convert_vggt_to_sonata(point_map_by_unprojection, images=images)
+Sonata_format, point_cloud_img1 = convert_vggt_to_sonata(point_map_by_unprojection, images=images)
+torch.save(point_cloud_img1, "point_cloud_img1.pt")
 
 torch.save(Sonata_format, "predictions.pt")
 print("\nSonata formatted predictions saved to predictions.pt \n")

vggt/vggt_visualisation.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-'''' Imports Sonata inference results and visualizes sementic segmentation '''
+"""' Imports Sonata inference results and visualizes sementic segmentation"""
 
 import numpy as np
 import open3d as o3d
@@ -20,16 +20,16 @@ import torch
 with torch.no_grad():
     torch.cuda.empty_cache()
 
-point = torch.load("sonata_points.pt")
-color = point.color.cpu().detach().numpy()
+point = torch.load("predictions.pt")
+color = point["color"].cpu().detach().numpy()
 print(point.keys())
 
 # Visualize
 pcd = o3d.geometry.PointCloud()
-points = torch.tensor(point.coord).cpu().detach().numpy()
+points = torch.tensor(point["coord"]).cpu().detach().numpy()
 # Inverse y axis
 points[:, 1] = -points[:, 1]
 pcd.points = o3d.utility.Vector3dVector(points)
 
-pcd.colors = o3d.utility.Vector3dVector(color / 255)
+pcd.colors = o3d.utility.Vector3dVector(color)
 o3d.visualization.draw_geometries([pcd])